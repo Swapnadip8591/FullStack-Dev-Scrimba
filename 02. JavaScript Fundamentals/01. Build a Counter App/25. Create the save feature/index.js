473344d05731aa4615ae12b9ce996ddf73db63ca
Swapnadip Mukherjee <swapnadip8591@gmail.com>
2025-11-17 00:19:55 +0530

    counter app done

---
+++ 02. JavaScript Fundamentals/01. Build a Counter App/25. Create the save feature/index.js
@@ -0,0 +1,19 @@
+// 1. Grab the save-el paragrah and store it in a variable called 
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.innerText = count
+}
+
+function save() {
+    // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
+    let saveMsg = count + " - "
+    // 3. Render the variable in the saveEl using innerText
+    // NB: Make sure to not delete the existing content of the paragraph
+    saveEl.innerText += saveMsg
+}
+
+
---
+++ 02. JavaScript Fundamentals/01. Build a Counter App/29. JavaScript challenges/01. Variables practice/index.js
@@ -0,0 +1,12 @@
+// Create two variables, firstName and lastName
+
+// Concatenate the two variables into a third variable called fullName
+
+// Log fullName to the console
+
+
+let firstName = "Swapnadip"
+let lastName = "Mukherjee"
+
+let fullName = firstName + " " + lastName
+console.log(fullName)
