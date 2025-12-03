c5c315755ef86495ede17fe59f26343fd7155a63
Swapnadip Mukherjee <swapnadip8591@gmail.com>
2025-12-03 16:15:47 +0530

    node js setup files

---
+++ 04. Node.js/01. Build a Node API/07. Routing and the req object/Exercise 2/server.js
@@ -0,0 +1,21 @@
+import http from 'node:http'
+
+const PORT = 8000
+
+
+const server = http.createServer( (req, res)=> {
+
+  console.log(req.url)
+
+/*
+Challenge:
+Check the ‘url’ property on the req object. 
+Only serve our string if it’s ‘/api’.
+*/
+
+  if (req.url === "/api" && req.method === 'GET'){
+      res.end('This is from the server')
+  }
+})
+
+server.listen(PORT, () => console.log(`Connected on port: ${PORT}`))
