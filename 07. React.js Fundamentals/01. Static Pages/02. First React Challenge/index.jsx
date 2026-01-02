75b427f8aacb395bdfe180e3a9ead4abdd263c89
Swapnadip Mukherjee <swapnadip8591@gmail.com>
2026-01-02 12:44:13 +0530

    React files updated

---
+++ 07. React.js Fundamentals/01. Static Pages/02. First React Challenge/index.jsx
@@ -0,0 +1,16 @@
+/**
+ * Challenge: Set up a React app from scratch again.
+ * This time, try rendering an unordered list with 2-3 list items inside
+ * with why you're excited to be learning React.
+ */
+import { createRoot } from "react-dom/client";
+
+const root = createRoot(document.getElementById('root'))
+
+root.render(
+    <ul>
+        <li>It's fun</li>
+        <li>Idk</li>
+        <li>Idk2</li>
+    </ul>
+)
